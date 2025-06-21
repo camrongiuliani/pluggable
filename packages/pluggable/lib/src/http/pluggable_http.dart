a9db1866cfb5522d8b5d7f4dda636b4e9c5c5cca
camrongiuliani <cgiuliani@savanainc.com>
2025-06-20 21:46:55 -0400

    feat: expose cache control

--- packages/pluggable/lib/src/http/pluggable_http.dart
+++ packages/pluggable/lib/src/http/pluggable_http.dart
@@ -17,4 +17,5 @@ export 'models/p_http_request.dart';
 export 'models/p_http_response.dart';
 export 'models/p_form_data.dart';
 export 'models/p_form_file.dart';
+export 'models/cache_control.dart';
 export 'models/http_status_code.dart';
